492541c8739c86a2342b56738091a169e4a093be
debashreeKingsmen <debashree.kingsmenrealty@gmail.com>
2025-05-02 00:55:33 +0530

    home screen post create

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,115 +1,158 @@
-import { Alert, StyleSheet, Text, View, Button , Pressable} from 'react-native'
-import React from 'react'
-import ScreenWrapper from '../../components/ScreenWrapper'
-import { useAuth } from '../../context/AuthContext'
-import { supabase } from '../../lib/supabase'
-import {hp,wp} from "../../helpers/common"
-import {theme} from "../../constants/theme"
-import Icon from "../../assets/icons"
-import { useRouter } from 'expo-router'
-import Avatar from '../../components/Avatar'
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Pressable,
+  FlatList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { useAuth } from "../../context/AuthContext";
+import { supabase } from "../../lib/supabase";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Icon from "../../assets/icons";
+import { useRouter } from "expo-router";
+import Avatar from "../../components/Avatar";
+import { fetchPost } from "../../services/postService";
+import PostCard from "../../components/PostCard";
+import Loading from "../../components/Loading";
+
+var limit = 0;
 
 const Home = () => {
-const router = useRouter()
-    const {user, setAuth} = useAuth();
-    // console.log("user details", user)
+  const router = useRouter();
+  const { user, setAuth } = useAuth();
+  const [posts, setPosts] = useState([]);
 
-    // const onLogout = async() => {
-    //     const {error} = await supabase.auth.signOut();
+  useEffect(() => {
+    getPosts();
+  });
 
-    //     if(error){
-    //         Alert.alert('sign out', "Error signing out")
-    //     }
-    // }
+  const getPosts = async () => {
+    //call the api here
+    limit = limit + 10;
+    let res = await fetchPost(limit);
+    // console.log("all post", res);
 
+    if (res.success) {
+      setPosts(res.data);
+    }
+  };
 
   return (
     <ScreenWrapper bg="white">
-     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>LinkUp</Text>
-        <View style={styles.icons}>
-          <Pressable onPress={() => router.push('notifications')}>
-            <Icon name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text}/>
-          </Pressable>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>LinkUp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Icon
+                name="heart"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
 
-          <Pressable onPress={() => router.push('newPost')}>
-            <Icon name="plus" size={hp(3.2)} strokeWidth={2} color={theme.colors.text}/>
-          </Pressable>
+            <Pressable onPress={() => router.push("newPost")}>
+              <Icon
+                name="plus"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
 
-          <Pressable onPress={() => router.push('profile')}>
-            <Avatar 
-            uri={user?.image}
-            size={hp(4.3)}
-            rounded={theme.radius.sm}
-            style={{borderWidth:2}}
-            />
-          </Pressable>
+            <Pressable onPress={() => router.push("profile")}>
+              <Avatar
+                uri={user?.image}
+                size={hp(4.3)}
+                rounded={theme.radius.sm}
+                style={{ borderWidth: 2 }}
+              />
+            </Pressable>
+          </View>
         </View>
+
+        <FlatList
+          data={posts}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.listStyle}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => 
+            <PostCard item={item} currentUser={user} router={router} />
+          }
+
+          ListFooterComponent={(
+            <View style={{marginVertical: posts.length==0?200: 30}}>
+              <Loading/>
+              </View>
+          )}
+        />
       </View>
-
-     </View>
       {/* <Button title='logout' onPress={onLogout}/> */}
-
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1,
   },
-  header:{
-    flexDirection: 'row',
-    alignItems:'center',
-    justifyContent: 'space-between',
-    marginBottom:10,
-    marginHorizontal: wp(4)
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4),
   },
-  title:{
+  title: {
     color: theme.colors.text,
     fontSize: hp(3.2),
-    fontWeight: theme.fonts.bold
+    fontWeight: theme.fonts.bold,
   },
-  avatarImage:{
+  avatarImage: {
     height: hp(4.3),
     width: hp(4.3),
     borderRadius: theme.radius.sm,
-    borderCurve: 'continuous',
+    borderCurve: "continuous",
     borderColor: theme.colors.gray,
-    borderWidth: 3
+    borderWidth: 3,
   },
-  icons:{
-   flexDirection:'row',
-   justifyContent:'center',
-   alignItems:'center',
-   gap:18
+  icons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
   },
-  listStyle:{
+  listStyle: {
     paddingTop: 20,
-    paddingHorizontal: wp(4)
+    paddingHorizontal: wp(4),
   },
-  noPosts:{
+  noPosts: {
     fontSize: hp(2),
-    textAlign:'center',
-    color: theme.colors.text
+    textAlign: "center",
+    color: theme.colors.text,
   },
-  pill:{
-    position: 'absolute',
+  pill: {
+    position: "absolute",
     right: -10,
     top: -4,
     height: hp(2.2),
     width: wp(2.2),
-    justifyContent: 'center',
-    alignItems:'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 20,
-    backgroundColor: theme.colors.roseLight
+    backgroundColor: theme.colors.roseLight,
   },
-  pillText:{
-    color:"white",
+  pillText: {
+    color: "white",
     fontSize: hp(1.2),
-    fontWeight: theme.fonts.bold
-  }
-})
+    fontWeight: theme.fonts.bold,
+  },
+});
--- services/postService.js
+++ services/postService.js
@@ -2,36 +2,54 @@ import { supabase } from "../lib/supabase";
 import { uploadFile } from "./imageService";
 
 export const createOrUpdatePost = async (post) => {
-    try {
-      // upload image
-      if (post.file && typeof post.file == 'object') {
-        let isImage = post?.file?.type == 'image';
-        let folderName = isImage ? 'postImages' : 'postVideos';
-        let fileResult = await uploadFile(folderName, post?.file?.uri , isImage)
+  try {
+    // upload image
+    if (post.file && typeof post.file == "object") {
+      let isImage = post?.file?.type == "image";
+      let folderName = isImage ? "postImages" : "postVideos";
+      let fileResult = await uploadFile(folderName, post?.file?.uri, isImage);
 
-        if(fileResult.success) post.file = fileResult.data;
-        else{
-            return fileResult;
-        }
+      if (fileResult.success) post.file = fileResult.data;
+      else {
+        return fileResult;
       }
+    }
 
-      const {data,error} = await supabase
-      .from('posts')
+    const { data, error } = await supabase
+      .from("posts")
       .upsert(post)
       .select()
-      .single()
+      .single();
 
-      if(error){
-        console.log("createPost error : ", error);
-        return {success: false, msg: 'Could not create your post'}
-      }
-
-      return {success: true , data:data}
-
-
-    } catch (error) {
-      console.log('createPost error: ', error);
-      return { success: false, msg: 'Could not create your post' };
+    if (error) {
+      console.log("createPost error : ", error);
+      return { success: false, msg: "Could not create your post" };
     }
+
+    return { success: true, data: data };
+  } catch (error) {
+    console.log("createPost error: ", error);
+    return { success: false, msg: "Could not create your post" };
   }
-  
+};
+
+export const fetchPost = async (limit=10) => {
+  try {
+    // upload image
+    const { data, error } = await supabase
+      .from("posts")
+      .select(`*, user: users(id,name,image)`)
+      .order("created_at", { ascending: false })
+      .limit(limit);
+
+    if (error) {
+      console.log("fetchPosts error: ", error);
+      return { success: false, msg: "Could not fetch the posts" };
+    }
+
+    return { success: true, data: data };
+  } catch (error) {
+    console.log("fetchPost error: ", error);
+    return { success: false, msg: "Could not fetch the post" };
+  }
+};
